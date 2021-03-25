f267e40a72a2bf66c1a37c630989dc848ab8851d
raphaelPloomes <69814263+raphaelPloomes@users.noreply.github.com>
2021-03-25 00:02:45 -0300

    [ADJUSTS]

--- config.js
+++ config.js
@@ -1,14 +1,15 @@
-const apiPost = 'https://discord.com/api/v8/channels/824008083487457342/messages';
-const apiGet = 'https://discord.com/api/v8/channels/824008083487457342/messages?limit=50';
+const chatId = '824008083487457342';
+const apiPost = `https://discord.com/api/v8/channels/${chatId}/messages`;
+const typingApi = `https://discord.com/api/v8/channels/${chatId}/typing`;
 const headers = {
     accept: '*/*',
     'accept-encoding': 'gzip, deflate, br',
     'accept-language': 'pt-BR',
     'content-type': 'application/json',
-    authorization: 'YOUR_AUTH',
+    authorization: 'NzQ0OTc2OTQ5MTU1MzMyMTA2.YFtT6A.mdJbH9mQBlhY4wIgHH5Lu5Pa-x4',
     'cookie': '__cfduid=de0e885576a7a30ae7ff46bf34b4d55181616437070; locale=pt-BR',
     'origin': 'https://discord.com',
-    'referer': 'https://discord.com/channels/689624343138795532/824008083487457342',
+    'referer': `https://discord.com/channels/689624343138795532/${chatId}`,
     'sec-ch-ua': '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
     'sec-ch-ua-mobile': '?0',
     'sec-fetch-dest': 'empty',
@@ -18,5 +19,7 @@ const headers = {
     'x-super-properties': 'eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzg5LjAuNDM4OS45MCBTYWZhcmkvNTM3LjM2IiwiYnJvd3Nlcl92ZXJzaW9uIjoiODkuMC40Mzg5LjkwIiwib3NfdmVyc2lvbiI6IjEwIiwicmVmZXJyZXIiOiJodHRwczovL3d3dy5nb29nbGUuY29tLyIsInJlZmVycmluZ19kb21haW4iOiJ3d3cuZ29vZ2xlLmNvbSIsInNlYXJjaF9lbmdpbmUiOiJnb29nbGUiLCJyZWZlcnJlcl9jdXJyZW50IjoiIiwicmVmZXJyaW5nX2RvbWFpbl9jdXJyZW50IjoiIiwicmVsZWFzZV9jaGFubmVsIjoic3RhYmxlIiwiY2xpZW50X2J1aWxkX251bWJlciI6ODAzMTQsImNsaWVudF9ldmVudF9zb3VyY2UiOm51bGx9'
     
 };
+const pokeBotId = '664508672713424926';
+const ourDiscordBotToken = 'ODI0Mzc4MTY0NDY0NTgyNjY2.YFuf_g.8T7YJWu13Vj_BtJfNGU6hUokrvg';
 
-export { apiPost, apiGet, headers };
+export { apiPost, chatId, pokeBotId, ourDiscordBotToken, typingApi, headers };
--- index.js
+++ index.js
@@ -1,74 +1,120 @@
 import axios from 'axios';
-import { apiPost, apiGet, headers } from './config.js';
+// import imageToBase64 from 'image-to-base64';
+import Discord from 'discord.js';
+import alert from 'alert';
+import { apiPost, chatId, pokeBotId, ourDiscordBotToken, headers, typingApi } from './config.js';
+const client = new Discord.Client();
 
 let intervalRef = null;
 let pbCount = 0;
-let gbCount = 0;
+// let gbCount = 0;
 let ubCount = 0;
-let mbCount = 0;
-let prbCount = 0;
-let findedPokesCount = 0;
+// let mbCount = 0;
+// let prbCount = 0;
+let foundCaptcha = false;
 
 const findPokemon = () => {
     return new Promise((resolve, reject) => {
-        axios.post(apiPost, { content: ";p" }, { headers }).then((res) => {
-            findedPokesCount++;
-            if (findedPokesCount === 10) {
-                resetBot();
-            }
+        tellDiscYreTyping().then(() => {
+            axios.post(apiPost, { content: ";p" }, { headers }).then((res) => {
+                resolve(res);
+            })
+        })
+    });
+};
+
+const throwPokeball = (pokeballType) => {
+    return new Promise((resolve, _reject) => {
+        tellDiscYreTyping().then(() => {
+            axios.post(apiPost, { content: pokeballType }, { headers }).then(res => {
+                shouldBuyPokeball();
+                resolve(res);
+            })
+        })
+    });
+};
+
+const tellDiscYreTyping = () => {
+    return new Promise((resolve, reject) => {
+        axios.post(typingApi, {}, { headers }).then((res) => {
             resolve(res);
         })
     });
 };
 
-const throwPokeball = () => {
-    return new Promise((resolve, reject) => {
-        axios.post(apiPost, { content: "pb" }, { headers }).then(res => {
-            pbCount++;
-            if (pbCount === 20) {
-                setTimeout(() => {
-                    buyPokeball();
-                }, 4000);
-            }
+const stopCaptchaCount = () => {
+    return new Promise((resolve, _reject) => {
+        axios.post(apiPost, { content: "stop captcha count" }, { headers }).then(res => {
             resolve(res);
         })
     });
 };
 
-const verifyCaptcha = () => {
-    return new Promise((resolve, reject) => {
-        axios.get(apiGet, { headers }).then((res) => {
-            console.log('RESSSSS', res.data);
-        })
-    });
-}
-
-const buyPokeball = () => {
-    return new Promise((resolve, reject) => {
-        axios.post(apiPost, { content: ";shop buy 1 20" }, { headers }).then(res => {
+const shouldBuyPokeball = () => {
+    if (pbCount === 20) {
+        setTimeout(() => {
+            buyPokeball(1, 20);
             pbCount = 0;
-            resolve(res);
+        }, 4000);
+    }
+    if (ubCount === 5) {
+        setTimeout(() => {
+            buyPokeball(3, 3);
+            ubCount = 0;
+        }, 4000);
+    }
+}
+
+const buyPokeball = (pokeballId, quantity) => {
+    return new Promise((resolve, _reject) => {
+        tellDiscYreTyping(() => {
+            axios.post(apiPost, { content: `;shop buy ${pokeballId} ${quantity}` }, { headers }).then(res => {
+                resolve(res);
+            })
         })
     });
-}
-
-const resetBot = () => {
-    findedPokesCount = 0;
-    clearInterval(intervalRef);
-    setTimeout(() => {
-        init();
-    }, 120000);
-}
-
-const init = () => {
-    intervalRef = setInterval(() => {
-        findPokemon().then((p) => {
-            // verifyCaptcha();
-            setTimeout(() => {
-                throwPokeball();
-            }, 4000)
-        })
-    }, 45000);
 };
 
-init();
+const getPokeballType = (text) => {
+    if (text.search('Common') !== -1 || text.search('Uncommon') !== -1) {
+        pbCount ++;
+        return 'pb';
+    } else if (text.search('Rare') !== -1 && text.search('Super') === -1) {
+        pbCount ++;
+        return 'pb';
+    } else if (text.search('Super Rare') !== -1) {
+        ubCount ++;
+        return 'ub';
+    } else if (text.search('Legendary') !== -1 || text.search('Shiny') !== -1) {
+        ubCount ++;
+        return 'ub';
+    }
+}
+
+const runWriteBot = () => {
+    intervalRef = setInterval(() => {
+        findPokemon();
+    }, 12500);
+};
+
+const runReadBot = () => {
+    client.on('message', msg => {
+        if (msg.channel.id === chatId && msg.author.id === pokeBotId) {
+            if (msg.content.search('captcha') !== -1 && !foundCaptcha) {
+                foundCaptcha = true;
+                clearInterval(intervalRef);
+                msg.channel.send("Olha só, temos um captcha aki, tchaom.");
+                alert('TEM CAPTCHA LA NO BOT SEU FDP');
+                stopCaptchaCount();
+            }
+            if (msg.content.search('catch') !== -1 && !foundCaptcha) {
+                const pokeballType = getPokeballType(msg.embeds[0].footer.text);
+                throwPokeball(pokeballType);
+            }
+        }
+    });
+    client.login(ourDiscordBotToken);
+}
+
+runWriteBot();
+runReadBot();
